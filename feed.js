2c36fda5bfd1ad9f74ceec8a8eecfb93cb5de466
wq308001 <wq308001@ucosh30.icu>
2026-02-12 21:54:38 +0300

    Change JSON fetch URLs to relative paths
    Updated fetch URLs for JSON data to relative paths.

--- feed.js
+++ feed.js
@@ -7,8 +7,8 @@ async function loadFeed() {
   try {
     // Загружаем оба JSON
     const [repoRes, postsRes] = await Promise.all([
-      fetch('https://raw.githubusercontent.com/wq308001/wq308001.github.io/refs/heads/main/file-repo.json'),
-      fetch('https://raw.githubusercontent.com/wq308001/wq308001.github.io/refs/heads/main/posts.json')
+      fetch('file-repo.json'),
+      fetch('posts.json')
     ]);
     
     const repoData = await repoRes.json();
@@ -220,4 +220,3 @@ function formatDate(dateStr) {
 }
 
 loadFeed();
-
